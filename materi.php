bc071da3c1a844386ff4084b2c897d16653f04e1
BrilyanNathanael <brilyannaathan@gmail.com>
2020-12-07 17:47:39 +0700

    initial-commit

---
+++ materi.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/materi.css">
+    <title>Materi</title>
+</head>
+<body>
+    <nav>
+        <a href="">
+            <img src="asset/Logo.png" alt="">
+        </a>
+        <div class="dropdown">
+            <button class="dropbtn">
+                <img src="asset/profile.png" alt="" width="20" height="20" style="border-radius: 100%;">
+                <p>Halo, Budi</p>
+                <img src="asset/navbar-dropdown.png" alt="" width="10" height="10">
+            </button>
+            <div class="dropdown-content">
+                <a href="profile.php">Profile Saya</a>
+                <a href="logout.php">Logout</a>
+            </div>
+        </div>
+    </nav>
+    
+    <section>
+        <h1>Lorem ipsum dolor sit.</h1>
+        <iframe src="https://www.youtube.com/embed/pDk94cOADQI" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" frameborder="0" allowfullscreen></iframe>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque dolor, neque maiores in a praesentium? Officia quo et incidunt facilis molestias laboriosam quaerat, dolorum recusandae quos culpa, similique hic nihil quidem, repudiandae eveniet suscipit voluptas. Eos quo tenetur repellendus similique.</p>
+    </section>
+
+    <footer>
+        <p>Copyright &copy; 2020 Online Learning</p>
+        <div class="medsos">
+            <a href="https://www.instagram.com/" target="_blank"><img src="asset/instagram-white.png" alt=""></a>
+            <a href="https://line.me/id/" target="_blank"><img src="asset/line-white.png" alt=""></a>
+            <a href="https://web.whatsapp.com/" target="_blank"><img src="asset/whatsapp-white.png" alt=""></a>
+        </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
